67e1c3327ea0ba37f0d740290f8455cc24ba0160
Alfonso Lamadrid <lamadrid.camacho@gmail.com>
2017-01-10 23:15:34 -0700

    added fetch, also: testing heaeders response

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // App is an invoked instance of express
 const app = require('express')();
+const fetch = require('fetch');
+
 let $port = 3000;
 
 // All request to root folder return a JSON msg with the client ip
 app.get('/', (req, res) => {
   return res.status(200).json({
-    // port: `${ process.env.PORT  || undefined }`,
-    ip: `${ req.headers['x-forwarded-for'] || req.ip }`
+    ip: `${ req.headers['x-forwarded-for'] }`
   });
 });
 
